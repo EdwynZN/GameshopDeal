efa8240d63f753fdb34121e91a7ffae25bc0f0fb
EdwynZN <edwinzn9@gmail.com>
2020-12-27 17:35:49 -0600

    Riverpod in favor of Provider

--- lib/screen/filter_screen.dart
+++ lib/screen/filter_screen.dart
@@ -1,155 +1,142 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:gameshop_deals/provider/filter_provider.dart';
+import 'package:flutter_riverpod/all.dart';
 import 'package:gameshop_deals/widget/filter/flag_filter.dart';
 import 'package:gameshop_deals/widget/filter/metacritic.dart';
 import 'package:gameshop_deals/widget/filter/price_slider.dart';
 import 'package:gameshop_deals/widget/filter/sort_by.dart';
 import 'package:gameshop_deals/widget/filter/steam_rating.dart';
-import 'package:gameshop_deals/widget/filter/stores.dart';
-import 'package:provider/provider.dart';
-import 'package:gameshop_deals/provider/deal_provider.dart';
-import 'package:gameshop_deals/model/store.dart';
 import 'package:gameshop_deals/widget/filter/order_by.dart';
-import 'package:collection/collection.dart';
+import 'package:gameshop_deals/widget/filter/stores.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gameshop_deals/model/filter_model.dart';
+import 'package:gameshop_deals/riverpod/filter_provider.dart';
 
 class FilterScreen extends StatelessWidget{
-  final bool isDrawer;
-
-  FilterScreen({this.isDrawer = false});
+  const FilterScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final DealProvider dealProvider = Provider.of<DealProvider>(context, listen: false);
-    final FilterProvider filterProvider = dealProvider.filter;
-    return isDrawer ?
-      Drawer(
-        child: FilterPage(filterProvider)//FilterPage(dealProvider.parameters),
-      ) :
-      SafeArea(
-        child: Material(
-          child: FilterPage(filterProvider) //FilterPage(dealProvider.parameters)
-        )
-      );
+    final bool isDrawer = ModalRoute.of(context).isFirst;
+    if(isDrawer) return const Drawer(child: const FilterPage());
+    return const SafeArea(
+      child: const Material(
+        child: const FilterPage() //FilterPage(dealProvider.parameters)
+      )
+    );
   }
 }
 
 class FilterPage extends StatelessWidget {
-  static final Function deepEq = const DeepCollectionEquality().equals;
-  final FilterProvider _filterProvider;
-
-  FilterPage(FilterProvider filter) : _filterProvider = filter.copyWith();
-
-  void _updateParameters(BuildContext context){
-    final DealProvider dealProvider = context.read<DealProvider>();
-    final bool eq = deepEq(dealProvider.filter.parameters, _filterProvider.parameters);
-    if(!eq) {
-      dealProvider.filter = _filterProvider;
-      dealProvider.requestListOfDeals;
-    }
-    Navigator.maybePop(context);
-  }
+  const FilterPage();
 
   @override
   Widget build(BuildContext context) {
-    final DealProvider dealProvider = Provider.of<DealProvider>(context, listen: false);
-    return Provider.value(
-      value: _filterProvider,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                    width: 1,
-                    color: Theme.of(context).dividerColor
-                ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                  width: 1,
+                  color: Theme.of(context).dividerColor
               ),
             ),
-            child: ListTile(
-              contentPadding: EdgeInsets.zero,
-              leading: CloseButton(),
-              title: Text('Filters', style: Theme.of(context).textTheme.headline4),
+          ),
+          child: ListTile(
+            contentPadding: const EdgeInsets.symmetric(horizontal: 4),
+            leading: CloseButton(),
+            title: Text('Filters', style: Theme.of(context).textTheme.headline4),
+            trailing: IconButton(
+              icon: Icon(Icons.refresh),
+              tooltip: 'Restart',
+              onPressed: () {
+                final StateController<Filter> filterCopy = context.read(filterProviderCopy);
+                final StateController<Filter> filter = context.read(filterProvider);
+                filterCopy.state = filter.state;
+              },
             ),
           ),
-          Expanded(
-              child: CustomScrollView(
-                slivers: <Widget>[
-                  SliverPadding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-                    sliver: SliverList(
-                      delegate: SliverChildListDelegate([
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 19),
-                          child: SizedBox(
-                              height: 38,
-                              child: OrderByWidget()
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 19),
-                          child: FlagFilterWidget(),
-                        ),
-                        Text('Price Range', style: Theme.of(context).textTheme.headline6),
-                        Padding(
-                            padding: const EdgeInsets.only(bottom: 19),
-                            child: PriceSlider()
-                        ),
-                        Text('Metacritic', style: Theme.of(context).textTheme.headline6),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 19),
-                          child: MetacriticFilter(),
-                        ),
-                        Text('Steam Rating', style: Theme.of(context).textTheme.headline6),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 19),
-                          child: SteamRating(),
-                        ),
-                        Text('Sort By', style: Theme.of(context).textTheme.headline6),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 19),
-                          child: SortByWidget(),
-                        ),
-                        Text('Stores', style: Theme.of(context).textTheme.headline6),
-                        FutureProvider<List<Store>>.value(
-                          value: dealProvider.requestListOfStores,
-                          child: StoreWidget(),
-                          catchError: (ctx, object) {
-                            Scaffold.of(ctx).showSnackBar(SnackBar(
-                              content: Text('No internet'),
-                              action: SnackBarAction(
-                                  label: 'retry',
-                                  onPressed: () => dealProvider.requestListOfStores
-                              ),
-                            ));
-                            return null;
-                          },
-                        ),
-                      ]),
+        ),
+        Expanded(
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                sliver: SliverList(
+                  delegate: SliverChildListDelegate([
+                    const Padding(
+                      padding: const EdgeInsets.only(bottom: 19),
+                      child: const SizedBox(
+                        height: 38,
+                        child: const OrderByWidget()
+                      ),
                     ),
-                  ),
-                ],
-              )
-          ),
-          Container(
-              decoration: BoxDecoration(
-                border: Border(
-                  top: BorderSide(
-                      width: 1,
-                      color: Theme.of(context).dividerColor
-                  ),
+                    const Padding(
+                      padding: const EdgeInsets.only(bottom: 19),
+                      child: const FlagFilterWidget(),
+                    ),
+                    Text('Price Range', style: Theme.of(context).textTheme.headline6),
+                    const Padding(
+                      padding: const EdgeInsets.only(bottom: 19),
+                      child: const PriceSlider()
+                    ),
+                    Text('Metacritic', style: Theme.of(context).textTheme.headline6),
+                    const Padding(
+                      padding: const EdgeInsets.only(bottom: 19),
+                      child: const MetacriticFilter(),
+                    ),
+                    Text('Steam Rating', style: Theme.of(context).textTheme.headline6),
+                    const Padding(
+                      padding: const EdgeInsets.only(bottom: 19),
+                      child: const SteamRating(),
+                    ),
+                    Text('Sort By', style: Theme.of(context).textTheme.headline6),
+                    const Padding(
+                      padding: const EdgeInsets.only(bottom: 19),
+                      child: const SortByWidget(),
+                    ),
+                    Text('Stores', style: Theme.of(context).textTheme.headline6),
+                    const StoreWidget()
+                  ]),
                 ),
               ),
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-              width: double.infinity,
-              child: OutlineButton(
-                onPressed: () => _updateParameters(context),
-                child: Text('Apply'),
-              )
+            ],
+          )
+        ),
+        Container(
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(
+                width: 1,
+                color: Theme.of(context).dividerColor
+              ),
+            ),
           ),
-        ],
-      ),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+          width: double.infinity,
+          child: const _ApplyButton()
+        ),
+      ],
+    );
+  }
+}
+
+class _ApplyButton extends StatelessWidget {
+  const _ApplyButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        final StateController<Filter> filterCopy = context.read(filterProviderCopy);
+        final StateController<Filter> filter = context.read(filterProvider);
+        if(filter.state == filterCopy.state) return;
+        filter.state = filterCopy.state.copy;
+        Navigator.maybePop(context);
+      },
+      child: Text('Apply'),
     );
   }
 }
