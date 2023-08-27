970624f874d429a55aa78c55077b178a4f1dbb97
EdwynZN <edwinzn9@gmail.com>
2023-08-27 01:21:25 -0600

    animate placeholder

--- lib/presentation/widgets/view_deals/thumb_image.dart
+++ lib/presentation/widgets/view_deals/thumb_image.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:gameshop_deals/provider/cache_manager_provider.dart';
@@ -34,12 +35,47 @@ class ThumbImage extends ConsumerWidget {
               ),
       cacheManager: ref.watch(cacheManagerProvider(cacheKey: cacheKeyDeals)),
       errorWidget: (_, __, ___) => const Icon(Icons.error),
-      placeholder: (_, __) => Container(
-        constraints: BoxConstraints.tight(const Size.square(35)),
-        padding: const EdgeInsets.all(2),
-        alignment: alignment,
-        child: const CircularProgressIndicator(strokeWidth: 2),
-      ),
+      placeholder: (_, __) => const _LoadingAsset(),
+    );
+  }
+}
+
+class _LoadingAsset extends HookWidget {
+  // ignore: unused_element
+  const _LoadingAsset({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final containerColor = theme.colorScheme.tertiary;
+    final initialColor = ElevationOverlay.applySurfaceTint(
+      theme.colorScheme.surface,
+      containerColor,
+      3.0,
+    );
+    final endColor = ElevationOverlay.applySurfaceTint(
+      theme.colorScheme.surface,
+      containerColor,
+      12.0,
+    );
+    final controller = useAnimationController(
+      duration: const Duration(seconds: 1),
+      animationBehavior: AnimationBehavior.preserve,
+      reverseDuration: const Duration(seconds: 1, milliseconds: 500),
+      keys: const [],
+    );
+    final animColor = useMemoized(
+      () => ColorTween(begin: initialColor, end: endColor).animate(controller),
+      [controller, initialColor, endColor],
+    );
+    useEffect(() {
+      controller.repeat(reverse: true);
+      return;
+    }, const []);
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) =>
+          ColoredBox(color: animColor.value ?? Colors.transparent),
     );
   }
 }
